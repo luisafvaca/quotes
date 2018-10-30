626e888a83ea0abf8cc418749631413a2bd373a9
Luisa Fernanda Vaca Correa <luisa.vaca@globant.com>
2018-10-30 13:07:16 -0500

    QT-removed mock data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,39 +6,6 @@ import Quotes from '../Quotes/Quotes';
 
 import './App.css';
 
-const testQuoteSaved = [
-  {
-    quote: "I don't have anything to say about other people's art and their work.",
-    author: "Dolly Parton",
-    cat: "art"
-  },
-  {
-    "quote": "The world is really figuring BSE out, and that's positive.",
-    "author": "Mike Johanns",
-    "cat": "positive"
-  },
-  {
-    quote: "Because of indifference, one dies before one actually dies.",
-    author: "Elie Wiesel",
-    cat: "death",
-  },
-  {
-    quote: "My girlfriend bought me a down jacket, she said it fit my personality.",
-    author: "Jay London",
-    cat: "dating",
-  },
-  {
-    quote: "For a while I was on the cover of every Sports Illustrated swimsuit issue, which was regarded as the pinnacle of success in America.",
-    author: "Elle Macpherson",
-    cat: "sports",
-  },
-  {
-    quote: "I'm equally guilty of using technology - I Twitter, I text people, I chat. But I think there's something strangely insidious about it that it makes us think we're closer when in fact we're not seeing each other, we're not connecting.",
-    author: "Jason Reitman",
-    cat: "technology",
-  }
-]
-
 class App extends Component {
   constructor(props){
     super(props)
