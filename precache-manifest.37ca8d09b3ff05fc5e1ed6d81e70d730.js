b21038fcd86acb5363a67bc1c6f83dc4b244f111
Luisa Fernanda Vaca Correa <luisa.vaca@globant.com>
2018-10-30 21:44:55 -0500

    Updates

---
+++ precache-manifest.37ca8d09b3ff05fc5e1ed6d81e70d730.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1a372811676cc4a90b0",
+    "url": "/quotes/static/js/runtime~main.f1a37281.js"
+  },
+  {
+    "revision": "5844695e50e5b7adf4ea",
+    "url": "/quotes/static/js/main.5844695e.chunk.js"
+  },
+  {
+    "revision": "c19f4ff54a51fdd4f4e6",
+    "url": "/quotes/static/js/1.c19f4ff5.chunk.js"
+  },
+  {
+    "revision": "5844695e50e5b7adf4ea",
+    "url": "/quotes/static/css/main.12bf9544.chunk.css"
+  },
+  {
+    "revision": "78c8845b3781fcdcc9c971f3c045970b",
+    "url": "/quotes/index.html"
+  }
+];
